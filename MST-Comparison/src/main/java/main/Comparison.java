1982e94f4f0a71e9a1b1477f2d44d415a7f8ec78
rpulkka <dreamspace2017@yandex.ru>
2019-02-05 13:47:43 +0200

    Simple Text UI added.

--- MST-Comparison/src/main/java/main/Comparison.java
+++ MST-Comparison/src/main/java/main/Comparison.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import data_management.FileHandler;
@@ -6,17 +5,18 @@ import data_management.GraphData;
 import algorithms.Prim;
 import algorithms.Boruvka;
 import algorithms.Kruskal;
-import domain.GraphCreator;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
- * The main class, where the algorithms will be executed, timed and compared. 
+ * The main class, where the algorithms will be executed, timed and compared.
  */
 public class Comparison {
-    
+
     private static GraphData graphData;
     private static FileHandler reader;
     private static Kruskal kruskal;
@@ -25,25 +25,44 @@ public class Comparison {
     //private static ReverseDelete reverseDelete;
     private static int numberOfVertices;
     private static int numberOfEdges;
-    
+
     public static void init() {
         reader = new FileHandler();
     }
-    
+
     public static void main(String args[]) throws IOException, URISyntaxException {
         init();
-        setGraph();
+        setGraph(askForFile());
         execute();
     }
-    
-    public static void setGraph() throws URISyntaxException, IOException {
-        URL url = Comparison.class.getResource("/testGraph3.csv");
+
+    public static URI askForFile() throws URISyntaxException {
+        URL url1 = Comparison.class.getResource("/");
+        URI uri1 = url1.toURI();
+        File folder = new File(uri1);
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].getName().contains(".csv") && (listOfFiles[i].getName().charAt(0) != '.')) {
+                System.out.println(listOfFiles[i].getName());
+            }
+        }
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a file name: ");
+        String fileName = "/" + scanner.next();
+        URL url = Comparison.class.getResource(fileName);
         URI uri = url.toURI();
+        return uri;
+    }
+
+    public static void setGraph(URI uri) throws URISyntaxException, IOException {
+//        URL url = Comparison.class.getResource("/testGraph3.csv");
+//        URI uri = url.toURI();
         graphData = new GraphData(new int[1000000], new int[1000000], new int[1000000]);
         reader.readFile(uri.getPath(), graphData);
 //        GraphCreator graphCreator = new GraphCreator();
 //        graphData = graphCreator.createGraph();
-        
+
         numberOfVertices = graphData.getNumberOfVertices();
         numberOfEdges = graphData.getNumberOfEdges();
         kruskal = new Kruskal(numberOfVertices);
@@ -51,7 +70,7 @@ public class Comparison {
         boruvka = new Boruvka(numberOfVertices, numberOfEdges);
         //reverseDelete = new ReverseDelete();
     }
-    
+
     public static void execute() {
         int kruskalResult = kruskal.execute(graphData);
         int primResult = prim.execute(graphData);
